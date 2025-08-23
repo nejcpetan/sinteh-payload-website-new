f363915855da3a10ae16a66f0597b67177dcadc8
nejcpetan <nejc.petan14@gmail.com>
2025-08-23 15:40:52 +0200

    ignore errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  typescript: {
+    // Ignore TypeScript errors during build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore ESLint errors during build
+    ignoreDuringBuilds: true,
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
